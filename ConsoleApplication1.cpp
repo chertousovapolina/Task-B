db6409297d2b2c42dc4124b47d1971e21d5dce7f
chertousovapolina <115408269+chertousovapolina@users.noreply.github.com>
2022-10-09 17:38:09 +0300

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,19 @@
+﻿// Chertousova PE
+//  Task 1
+// 9.10.2022
+#include <iostream>
+using namespace std; 
+int main()
+{
+	int w;
+	cin >> w;
+	if (w % 2 == 0) {
+		// тело
+		// выполнится только если w % 2 == 
+		cout << "YES";
+	}
+	else {// только если w % 2 !=0
+		cout << "NO";
+	}
+
+}
